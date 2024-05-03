dadbbef533e58ca537467d55b27f955da0188c11
universalheart <168195506+universalheart@users.noreply.github.com>
2024-05-03 16:09:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 29707621
-api_hash = "a1f31eeb31a0b9e36f551da6b8b85752"
-bot_token = "6634971116:AAGhU6IQBREjXdliqJr9CGh1ZCLpoYbGP5o"
-auth_users = "6707073614, 1406300051, 6801378994, 870101201"
+api_id = 28660719
+api_hash = "34ade52147ebcaf4bc0b88aa3dfc7e1a"
+bot_token = "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8"
+auth_users = "6990313439"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6707073614"
+osowner_users = "6990313439"
 owner_users = [int(num) for num in osowner_users.split(",")]
