0ccedc4eadeaa9e064841a92a4a85a5a080a6a26
universalheart <168195506+universalheart@users.noreply.github.com>
2024-05-03 16:13:51 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 28660719
 api_hash = os.environ.get("API_HASH", "34ade52147ebcaf4bc0b88aa3dfc7e1a")
-bot_token = os.environ.get("BOT_TOKEN")
+bot_token = os.environ.get("BOT_TOKEN", "7001245667:AAGm3VkNigvYgO5nrB9Civflz9lop0B91d8")
 auth_users = os.environ.get("AUTH_USERS", "6990313439")
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = os.environ.get("OWNER_USERS", "6990313439")
